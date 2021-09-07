4cb581ca26a11c50aa181052cf7591def20987e0
BrunoCostaPrado <89091606+BrunoCostaPrado@users.noreply.github.com>
2021-09-07 17:37:47 -0300

    Delete podman.sh

--- podman.sh
+++
@@ -1,8 +0,0 @@
-. /etc/os-release
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install podman
-sudo apt install cockpit -y
