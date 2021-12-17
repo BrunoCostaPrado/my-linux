a8f317706e06dd21d8bb76186e5bb9968406bf47
Bruno <brunocostaprado@gmail.com>
2021-12-17 02:10:00 -0300

    .

--- prog.sh
+++ prog.sh
@@ -1,3 +1,6 @@
-sudo apt install gedit steam python -y 
 sudo add-apt-repository multiverse -y
+sudo add-apt-repository ppa:lutris-team/lutris
+sudo apt update
+sudo apt install gedit steam lutris -y 
+
 
---
+++ programas.sh
@@ -0,0 +1,24 @@
+sudo apt install wget apt-transport-https -y
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
+
+
+sudo rm /etc/apt/preferences.d/nosnap.pref
+sudo apt update
+sudo apt install snapd
+sudo snap install mysql-workbench-community 
+sudo apt-get install mysql-server
+
+
+sudo apt install php -y
+sudo apt install python -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install golang-go 
+
+#Ruby#
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
+\curl -sSL https://get.rvm.io | bash -s stable --rails
