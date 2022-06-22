859593bac60ae08a7a9951b1c615038f4697f04c
Bruno Costa Prado <89091606+BrunoCostaPrado@users.noreply.github.com>
2022-06-22 18:06:47 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -21,3 +21,11 @@ sudo apt install golang-go
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 \curl -sSL https://get.rvm.io | bash -s stable --rails
+
+# Using Ubuntu
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt install -y htop
+
+sudo apt update 
