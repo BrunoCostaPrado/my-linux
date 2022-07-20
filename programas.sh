6742f40faa50dacdcf3039ab881b84f9de2d809c
Bruno Costa Prado <89091606+BrunoCostaPrado@users.noreply.github.com>
2022-07-20 01:47:30 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -50,3 +50,13 @@ sudo apt-key add gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+
+
+#Podman
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/Release.key" | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install podman
+podman --version
