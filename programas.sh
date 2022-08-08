6602c518145fcfdb66c049f019dd15ee36fbf05c
Bruno Costa Prado <89091606+BrunoCostaPrado@users.noreply.github.com>
2022-08-08 19:21:44 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -25,9 +25,14 @@ gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D69
 \curl -sSL https://get.rvm.io | bash -s stable
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 
-# Using Ubuntu
+# NodeJS
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+#Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
 
 
 sudo apt update 
