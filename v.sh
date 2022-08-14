7216eb93367dd22b6ea5453caf8e69ddaa61ad5f
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-08-14 02:25:10 -0300

    edit

--- v.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt install -y libwebkit2gtk-4.0-dev \
-    build-essential \
-    curl \
-    wget \
-    libssl-dev \
-    libgtk-3-dev \
-    libayatana-appindicator3-dev \
-    librsvg2-dev 
