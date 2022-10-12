441da54ba0a1c458d6e4911d4509ef6d5f73ebf1
Bruno Costa Prado <89091606+BrunoCostaPrado@users.noreply.github.com>
2022-10-12 20:43:57 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -32,20 +32,13 @@ sudo apt-get install -y nodejs
 #Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
+sudo apt update && sudo apt install yarn -y
 
 
 sudo apt update 
 
 sudo apt update
-sudo apt install -y libwebkit2gtk-4.0-dev \
-    build-essential \
-    curl \
-    wget \
-    libssl-dev \
-    libgtk-3-dev \
-    libayatana-appindicator3-dev \
-    librsvg2-dev 
+sudo apt install -y libwebkit2gtk-4.0-dev     build-essential     curl     wget     libssl-dev libgtk-3-dev libayatana-appindicator3-dev ibrsvg2-dev
     
     
     
