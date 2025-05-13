ec8f497e16eebea5ad1ccb5037a37711627af480
Bruno Costa Prado <89091606+BrunoCostaPrado@users.noreply.github.com>
2025-05-13 00:39:27 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,26 @@ echo "Pulando VMware-Tools"
 fi
 clear
 
+
+echo "Deseja instalar Fish"
+echo "1 para sim"
+echo "0 para não"
+read in
+clear
+
+if [ $in -gt "0" ]
+then
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
+sudo apt update
+sudo apt install fish
+else 
+echo "Pulando Fish"
+fi
+clear
+
+
+
 echo "Deseja atualizar?"
 
 echo "1 para sim"
